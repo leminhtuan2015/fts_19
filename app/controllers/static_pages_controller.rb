a28f7a7bb913429e0d0d699b8cbce0d1478b3143
leminhtuan2015 <leminhtuan2015@gmail.com>
2015-03-26 16:52:27 +0700

    search-remore-ajax

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,5 @@
 class StaticPagesController < ApplicationController
+
   def home
     if user_signed_in? 
       if current_user.admin?
@@ -6,16 +7,24 @@ class StaticPagesController < ApplicationController
       else
         @subjects = Subject.all
         @exam = Exam.new
-        if params[:search].nil?
+
+        if params[:search].nil? && params[:search_remote].nil?
           @exams = current_user.exams.paginate page: params[:page], per_page: 10
-        else
-          if params[:search].empty?
-            @exams = current_user.exams.paginate page: params[:page], per_page: 10
-          else
-            @exams = current_user.exams.search(params[:search]).paginate page: params[:page], per_page: 10
-          end
+        elsif params[:search_remote].nil?
+          search params[:search]
+        elsif params[:search].nil?
+          search params[:search_remote]
         end
       end
+    end
+  end
+
+  private
+  def search(search_query)
+    if search_query.empty?
+      @exams = current_user.exams.paginate page: params[:page], per_page: 10
+    else
+      @exams = current_user.exams.search(search_query).paginate page: params[:page], per_page: 10
     end
   end
 end
